39cd8397aef7ee1fa544f184c476709c438b6b91
Magnus Jonsson <fot@fot.nu>
2017-08-01 23:36:14 +0200

    Added ts-loader in webpack, added package.json with deps.

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,15 @@
 module.exports = {
-  entry: ['./dist/morse.js' ],
+  entry: ['./src/morse.ts' ],
   output: {
     library: 'MorsePlayer',
     filename: 'morse_code_player.js'
+  },
+  resolve: {
+    extensions: ['.ts']
+  },
+  module: {
+    rules: [
+      { test: /\.ts$/, loader: 'ts-loader' }
+    ]
   }
 }
